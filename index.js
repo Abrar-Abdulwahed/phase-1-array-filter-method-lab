c15dd2f6eecc9eb2c3490f83b21199ca1e1c86e6
A-Abd <abrar.abdulwahed@gmail.com>
2022-12-31 23:58:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 function findMatching(drivers, condition){
-    const matches =  drivers.filter((elem) => {
+    return drivers.filter((elem) => {
         return elem.toLowerCase() === condition.toLowerCase();
     });
-    return matches;
 }
 
 function fuzzyMatch(drivers, condition){
-    const matches =  drivers.filter((elem) => {
+    return drivers.filter((elem) => {
         return elem.startsWith(condition);
     });
-    return matches;
 }
 
 function matchName(drivers, condition){
-    const matches =  drivers.filter((elem) => {
+    return drivers.filter((elem) => {
         return elem.name === condition;
     });
-    return matches;
 }
