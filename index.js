1f81820d17cdf6e45e91e138d3ab3b77a337d2c4
A-Abd <abrar.abdulwahed@gmail.com>
2022-12-31 19:45:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, condition){
+    const matches =  drivers.filter((elem) => {
+        return elem.toLowerCase() === condition.toLowerCase();
+    });
+    return matches;
+}
+
+function fuzzyMatch(drivers, condition){
+    const matches =  drivers.filter((elem) => {
+        return elem.startsWith(condition);
+    });
+    return matches;
+}
+
+function matchName(drivers, condition){
+    const matches =  drivers.filter((elem) => {
+        return elem.name === condition;
+    });
+    return matches;
+}
